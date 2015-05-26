2eb8c28462b3b36c158f426781b7b2ee21375859
David Faulkenberry <david.faulkenberry@gmail.com>
2015-05-26 17:27:06 -0400

    Better variable declaration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   include DateAndTimeMethods
   helper_method DateAndTimeMethods.instance_methods
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  attr_accessor :current_user
   protect_from_forgery with: :exception
   before_action :set_current_user, unless: -> { params[:skip_current_user] }
   layout 'application'
