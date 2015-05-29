fbe9166d08c2354f75942c2232195ac56e8eeb8a
David Faulkenberry <david.faulkenberry@gmail.com>
2015-05-29 14:43:40 -0400

    Pending tests for ApplicationHelper#show_message
    There are a couple of tricky things in terms of providing test coverage here. First, it'll have to be done in the context of a controller action, so that flash is defined. Second, we'll have to fake configuration values so that we're not relying on the git-ignored application.yml file of whatever version of this app is running the test.
    
    I'll have to come back to this later.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
+  describe 'show_message' do
+    context 'message present in configuration' do
+      it 'stores specified message in flash'
+    end
+    context 'message not present in configuration' do
+      context 'default specified' do
+        it 'stores given default message in flash'
+      end
+      context 'default not specified' do
+        it 'raises an ArgumentError'
+      end
+    end
+  end
   describe 'text' do
     before :each do
       @site_text = create :site_text
