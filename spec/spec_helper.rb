af731471a98e5f1595b32dde3f668725a9adf024
David Faulkenberry <david.faulkenberry@gmail.com>
2015-06-03 12:03:07 -0400

    Only reporting coverage to codeclimate in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'codeclimate-test-reporter'
 require 'factory_girl_rails'
 require 'simplecov'
 
-CodeClimate::TestReporter.start
+CodeClimate::TestReporter.start if ENV['CI']
 SimpleCov.start 'rails'
 SimpleCov.start do
   add_filter '/config/'
