81c7c98752d37be1c3e4d50c3b88b069de17f51a
David Faulkenberry <david.faulkenberry@gmail.com>
2015-06-03 13:17:49 -0400

    Rubocop is green for lib

--- lib/date_and_time_methods.rb
+++ lib/date_and_time_methods.rb
@@ -1,10 +1,9 @@
+# Generic date and time methods, included in ApplicationController by default.
 module DateAndTimeMethods
-
   def format_date_time datetime, options = {}
     case options[:format]
     when :iCal then datetime.utc.strftime '%Y%m%dT%H%M%SZ'
-    else datetime.strftime '%A, %B %e, %Y - %l:%M %P'     
+    else datetime.strftime '%A, %B %e, %Y - %l:%M %P'
     end
   end
-
 end
