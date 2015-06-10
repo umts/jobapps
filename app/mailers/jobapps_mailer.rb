609cdbedfe7bfb6667d27d66605d2a92d6fc7cf9
Karin Eichelman <keichelman@gmail.com>
2015-06-10 09:49:36 -0400

    made some changes to mailer

--- app/mailers/jobapps_mailer.rb
+++ app/mailers/jobapps_mailer.rb
@@ -9,9 +9,20 @@ class JobappsMailer < ActionMailer::Base
          subject: 'Interview Confirmation'
   end
 
-  # def application_denial application
-  # @application = application
-  # @user = application.user
-  # mail to: @user.email, subject: 'Application Denial'
-  # end
+  def interview_rescheduled(interview)
+    @interview = interview
+    @user = interview.user
+    mail from: configured_value([:email, :default_from], default: false),
+         to: @user.email,
+         subject: 'Interview Rescheduled'
+  end
+
+
+   def application_denial(application_record)
+     @application_record = application_record
+     @user = application_record.user
+     mail from: configured_value([:email, :default_from], default: false),
+          to: @user.email,
+          subject: 'Application Denial'
+   end
 end
