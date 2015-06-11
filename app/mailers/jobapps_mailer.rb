d70991786d4627f2bbe24a946be626dd20af29ba
David Faulkenberry <david.faulkenberry@gmail.com>
2015-06-11 14:40:47 -0400

    Removing some duplication from JobappsMailer
    Addresses #45.

--- app/mailers/jobapps_mailer.rb
+++ app/mailers/jobapps_mailer.rb
@@ -2,27 +2,27 @@ class JobappsMailer < ActionMailer::Base
   include ApplicationConfiguration
   helper_method :configured_value
 
+  #for some reason the configured_value method doesn't work here
+  default from: CONFIG[:email][:default_from]
+
   def application_denial(application_record)
     @application_record = application_record
     @user = application_record.user
-    mail from: configured_value([:email, :default_from]),
-         to: @user.email,
+    mail to: @user.email,
          subject: 'Application Denial'
   end
 
   def interview_confirmation(interview)
     @interview = interview
     @user = interview.user
-    mail from: configured_value([:email, :default_from]),
-         to: @user.email,
+    mail to: @user.email,
          subject: 'Interview Confirmation'
   end
 
   def interview_reschedule(interview)
     @interview = interview
     @user = interview.user
-    mail from: configured_value([:email, :default_from]),
-         to: @user.email,
+    mail to: @user.email,
          subject: 'Interview Rescheduled'
   end
 
@@ -30,8 +30,7 @@ class JobappsMailer < ActionMailer::Base
     @user = user
     @location = location
     @description = description
-    mail from: configured_value([:email, :default_from]),
-         to:   configured_value([:email, :site_text_request_address]),
+    mail to: configured_value([:email, :site_text_request_address]),
          subject: "Site text request from #{user.full_name}"
   end
 end
