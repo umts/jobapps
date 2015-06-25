59d1d8d5c33b2704f90f77dad931df7ea070709c
Karin Eichelman <keichelman@gmail.com>
2015-06-25 11:03:03 -0400

    Wrote some more view tests and modified a method in the interview model

--- spec/views/application_records/show_spec.rb
+++ spec/views/application_records/show_spec.rb
@@ -16,14 +16,14 @@ describe 'application_records/show.haml' do
       end
       it 'contains a form to reject the application and provide a staff note' do
         render
-        action_path = review_application_record_path(@record, accepted: false)
+        action_path = review_application_record_path @record, accepted: false
         expect(rendered).to have_form action_path, :post do
           with_text_area :staff_note
         end
       end
-      it 'contains a form to accept the application and schedule an interview' do
+      it 'contains form to accept the application and schedule an interview' do
         render
-        action_path = review_application_record_path(@record, accepted: true)
+        action_path = review_application_record_path @record, accepted: true
         expect(rendered).to have_form action_path, :post do
           with_text_field 'interview[location]'
           with_tag 'input#interview_scheduled.datetimepicker'
@@ -61,21 +61,23 @@ describe 'application_records/show.haml' do
           end
           it 'contains a link to a calendar export file' do
             render
+            action_path = interview_path @interview, format: :ics
             expect(rendered).to have_tag '.export_link' do
-              with_tag 'a', with:{href: interview_path(@interview, format: :ics)}
+              with_tag 'a', with: { href: action_path }
             end
           end
           it 'contains a form to reschedule an interview' do
             render
-            expect(rendered).to have_form reschedule_interview_path(@interview), :post do
+            action_path = reschedule_interview_path @interview
+            expect(rendered).to have_form action_path, :post do
               with_tag 'input#scheduled.datetimepicker'
               with_text_field :location, @interview.location
             end
           end
           it 'contains a form to mark interview as complete' do
             render
-            expect(rendered).to have_form complete_interview_path(@interview), :post do
-            end
+            action_path = complete_interview_path @interview
+            expect(rendered).to have_form action_path, :post
           end
         end
         context 'interview is complete' do
@@ -121,9 +123,9 @@ describe 'application_records/show.haml' do
           end
           it 'contains text saying that the interview is scheduled' do
             render
-            expect(rendered).to include 'is scheduled for'
+            expect(rendered).to include 'interview is scheduled for'
           end
-          it 'contains the time and location of the interview' do
+          it 'contains the time, date and location of the interview' do
             render
             expect(rendered).to include @interview.information
           end
@@ -133,16 +135,25 @@ describe 'application_records/show.haml' do
             @interview.update completed: true
             assign :interview, @interview
           end
-          it 'contains text saying that the interview has occurred'
-          it 'contains the time and date when the interview is scheduled'
+          it 'contains text saying that the interview has occurred' do
+            render
+            expect(rendered).to include 'interview occurred on'
+          end
+          it 'contains the time, date and location of the interview' do
+            render
+            expect(rendered).to include @interview.information
+          end
         end
       end
       context 'interview not present' do
         before :each do
-          @interview.delete
+          @record.interview.delete if @record.interview.present?
           assign :interview, nil
         end
-        it 'contains text saying application has been denied'
+        it 'contains text saying application has been denied' do
+          render
+          expect(rendered).to include 'application has been denied'
+        end
       end
     end
   end
