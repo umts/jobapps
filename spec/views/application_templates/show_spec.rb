9262b8086a584e19c9b2fc922520eaa9d7f58ed0
Karin Eichelman <keichelman@gmail.com>
2015-06-26 10:48:17 -0400

    finished view tests for application_template

--- spec/views/application_templates/show_spec.rb
+++ spec/views/application_templates/show_spec.rb
@@ -3,22 +3,28 @@ include RSpecHtmlMatchers
 
 describe 'application_templates/show.haml' do
   before :each do
-    @record = create :application_record
+    assign :template, create(:application_template)
   end
   context 'current user is student' do
     before :each do
-      when_current_user_is :student, view: true 
+      when_current_user_is :student, view: true
     end
     it 'contains a form to submit the application' do
       render
-      action_path = application_template_path @record
+      action_path = application_records_path
       expect(rendered).to have_form action_path, :post
     end
-  end  
+  end
   context 'current user is staff' do
     before :each do
       when_current_user_is :staff, view: true
     end
-    it 'shows the application form with no submit button'
+    it 'shows the application form with no submit button' do
+      render
+      action_path = application_records_path
+      expect(rendered).to have_form action_path, :post do
+        without_tag :input, with: { type: 'submit' }
+      end
+    end
   end
 end
