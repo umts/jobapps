cd62b280dffe78840aeda6a8e3d508a5749b2969
Karin Eichelman <keichelman@gmail.com>
2015-06-26 12:01:39 -0400

    started more view tests

---
+++ spec/views/layouts/application_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+include RSpecHtmlMatchers
+
+describe 'layouts/application.haml' do
+  context 'current user is present' do
+    before :each do
+      when_current_user_is (create :user, view: true)
+    end
+    context 'current user is staff' do
+      when_current_user_is :staff, view: true
+    end
+  end
+  context 'message present in flash' do
+  end 
+  context 'errors present in flash' do
+  end
+end
