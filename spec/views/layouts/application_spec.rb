9a61efbe04ee146a7d6ae2e0a2bfe619072e46e3
David Faulkenberry <david.faulkenberry@gmail.com>
2015-06-26 12:27:42 -0400

    Running the whole suite and fixing issues

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -4,14 +4,16 @@ include RSpecHtmlMatchers
 describe 'layouts/application.haml' do
   context 'current user is present' do
     before :each do
-      when_current_user_is (create :user, view: true)
+      when_current_user_is(create :user, view: true)
     end
     context 'current user is staff' do
-      when_current_user_is :staff, view: true
+      before :each do
+        when_current_user_is :staff, view: true
+      end
     end
   end
   context 'message present in flash' do
-  end 
+  end
   context 'errors present in flash' do
   end
 end
