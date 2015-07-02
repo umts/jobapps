73307bbd0e3c0a54da428fda53c658c87e44a41e
Karin Eichelman <keichelman@gmail.com>
2015-07-02 13:01:49 -0400

    wrote some more view tests

--- spec/views/application_templates/edit_spec.rb
+++ spec/views/application_templates/edit_spec.rb
@@ -5,13 +5,13 @@ describe 'application_templates/edit.haml' do
   before :each do
     @template = create :application_template
     @top_question = create :question, application_template: @template
-    @middle_question = create :question, application_template: @template 
+    @middle_question = create :question, application_template: @template
     @bottom_question = create :question, application_template: @template
   end
   it 'includes a link to view application' do
     render
     action_path = application_template_path @template
-    expect(rendered).to have_tag 'a', with: { href: action_path } 
+    expect(rendered).to have_tag 'a', with: { href: action_path }
   end
   it 'contains a form with questions' do
     render
@@ -20,7 +20,9 @@ describe 'application_templates/edit.haml' do
   end
   it 'has a prompt that is required' do
     render
-    expect(rendered).to have_tag 'textarea', with: { required: 'required', name: 'question[prompt]' }
+    expect(rendered).to have_tag 'textarea',
+                                 with: { required: 'required',
+                                         name: 'question[prompt]' }
   end
   it 'contains options to select data type' do
     render
@@ -28,7 +30,9 @@ describe 'application_templates/edit.haml' do
   end
   it 'contains a check box to determine whether question is required' do
     render
-    expect(rendered).to have_tag 'input', with: { type: 'checkbox', name: 'question[required]' }
+    expect(rendered).to have_tag 'input',
+                                 with: { type: 'checkbox',
+                                         name: 'question[required]' }
   end
   it 'has a submit button' do
     render
@@ -52,12 +56,12 @@ describe 'application_templates/edit.haml' do
   it 'does not have a button to move up if top question' do
     render
     action_path = move_question_path(@top_question, direction: :up)
-    expect(rendered).not_to have_form action_path, :post 
+    expect(rendered).not_to have_form action_path, :post
   end
   it 'has a button to move down if top question' do
     render
     action_path = move_question_path(@top_question, direction: :down)
-    expect(rendered).to have_form action_path, :post 
+    expect(rendered).to have_form action_path, :post
   end
   it 'does not have a button to move down if bottom question' do
     render
