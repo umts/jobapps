083479aed769185c51e009d82b17986be88e0458
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-15 15:52:37 -0400

    Removing unneeded tests

--- spec/views/dashboard/application_templates_spec.rb
+++ spec/views/dashboard/application_templates_spec.rb
@@ -24,23 +24,6 @@ describe 'dashboard/_application_templates.haml' do
     end
   end
   context 'there are no application templates for the positions listed' do
-    before :each do
-      # seems dumb to make an application template where there are none
-      # but I need to reference paths that don't exist.
-      # assigning it to nil is not necessary but.. test still passes.
-      @application_template = create :application_template
-      assign :application_template, nil
-    end
-    it 'does not include a link to edit the application template' do
-      render
-      action_path = edit_application_template_path @application_template
-      expect(rendered).not_to have_tag 'a', with: { href: action_path }
-    end
-    it 'does not include a link to view the application template' do
-      render
-      action_path = application_template_path @application_template
-      expect(rendered).not_to have_tag 'a', with: { href: action_path }
-    end
     it 'contains a link to create an application' do
       render
       action_path = new_application_template_path(position_id: @position.id)
