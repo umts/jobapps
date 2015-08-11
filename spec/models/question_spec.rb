e9306af4429e70a3448afd8e02cf17984e19f175
Karin Eichelman <keichelman@gmail.com>
2015-08-11 09:52:38 -0400

    removed move question tests from question model spec. made other tests pass. will fail rspec for other files.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Question do
+  before :each do
+    @template = create :application_template
+  end
   context 'data_type identification methods' do
     before :each do
-      @date = create :question, data_type: 'date'
-      @explanation = create :question, data_type: 'explanation'
-      @heading = create :question, data_type: 'heading'
+      @date = create :question, data_type: 'date', application_template: @template
+      @explanation = create :question, data_type: 'explanation', application_template: @template
+      @heading = create :question, data_type: 'heading', application_template: @template
     end
 
     describe 'date?' do
@@ -33,44 +36,6 @@ describe Question do
       end
       it 'returns false if data type is not heading' do
         expect(@date.heading?).to eql false
-      end
-    end
-  end
-  describe 'move' do
-    before :each do
-      @template = create :application_template
-      @question_above = create :question,
-                               application_template: @template,
-                               number: 2
-      @question = create :question,
-                         application_template: @template,
-                         number: 3
-      @question_below = create :question,
-                               application_template: @template,
-                               number: 4
-    end
-    context 'moving up' do
-      it 'decreases the question number by 1' do
-        @question.move :up
-        expect(@question.reload.number).to eql 2
-      end
-      it 'increases the number of the question above by 1' do
-        @question.move :up
-        expect(@question_above.reload.number). to eql 3
-      end
-      it 'does nothing if there is no question to swap with' do
-        expect { @question_above.move :up }
-          .not_to change { @question_above.number }
-      end
-    end # of context 'moving up'
-    context 'moving down' do
-      it 'increases the question number by 1' do
-        @question.move :down
-        expect(@question.reload.number).to eql 4
-      end
-      it 'decreases the number of the question below by 1' do
-        @question.move :down
-        expect(@question_below.reload.number). to eql 3
       end
     end
   end
