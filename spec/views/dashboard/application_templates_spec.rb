e069117fb01b06240bb01fc44a4d56675fe62d63
Karin Eichelman <keichelman@gmail.com>
2015-08-11 12:40:17 -0400

    view specs half done for application template partial

--- spec/views/dashboard/application_templates_spec.rb
+++ spec/views/dashboard/application_templates_spec.rb
@@ -7,20 +7,39 @@ describe 'dashboard/_application_templates.haml' do
     assign :departments, Array(@department)
     @position = create :position, department: @department
     assign :positions, Array(@position)
+    when_current_user_is :staff, view: true
   end
   context 'there are application templates present' do
     before :each do
-      @application_template = create :application_template, position: @position
+      @template = create :application_template, position: @position
+      assign :templates, Array(@template)
     end
     it 'contains a link to view the application template' do
       render
-      action_path = application_template_path @application_template
+      action_path = application_template_path @template
       expect(rendered).to have_tag 'a', with: { href: action_path }
     end
-    it 'contains a link to edit the application template' do
-      render
-      action_path = edit_application_template_path @application_template
-      expect(rendered).to have_tag 'a', with: { href: action_path }
+    context 'there are no existing drafts for that application' do
+      it 'contains a link to edit the application' do
+        render
+        action_path = new_application_template_draft_path(application_template_id: @template.id)
+        expect(rendered).to have_tag 'a', with: { href: action_path }
+      end
+    end
+    context 'there are existing drafts for that application' do
+      before :each do
+        @draft = create :application_template_draft, application_template: @template
+      end
+      it 'has a link to resume editing the saved draft' do
+        render
+        action_path = edit_application_template_draft_path @draft
+        expect(rendered).to have_tag 'a', with: { href: action_path }
+      end
+      it 'has a button to discard the saved draft' do
+        render
+        action_path = application_template_draft_path @draft
+        expect(rendered).to have_form action_path, :delete
+      end
     end
   end
   context 'there are no application templates for the positions listed' do
