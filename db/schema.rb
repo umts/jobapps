148bac5c8e440f78846d41fbcf64691f27fed6fe
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-06 17:14:27 -0500

    Added the description field of the Interview ical export. It should populate with the absolute path of the ApplicationRecord for that application

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20151208200954) do
-=======
 ActiveRecord::Schema.define(version: 20151217172608) do
->>>>>>> umts/master
 
   create_table "application_drafts", force: :cascade do |t|
     t.integer  "application_template_id", limit: 4
