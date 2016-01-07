c85d4938b4888bb9376b010d46c29d6e432cabe0
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-07 12:36:37 -0500

    Reworked how tests deal with newlines and took out some superfluous stuff. Ran full test suite.

--- spec/views/interviews/ical_export_spec.rb
+++ spec/views/interviews/ical_export_spec.rb
@@ -8,19 +8,17 @@ describe 'interviews/interview.ics.erb' do
   
   it 'Passes the correct field properties to the ics object' do
     render
-    @lines = {};
+    @lines = {}
     rendered.each_line do |line|
       key, value = line.split ':', 2
-      @lines[key] = value
+      @lines[key] = value.strip
     end
     @lines.each do |key, value|
-      puts key + ':'
-      puts value
     end
-    expect(@lines['DTSTART']).to eql ((format_date_time @interview.scheduled, format: :iCal) + "\n")
-    expect(@lines['DESCRIPTION']).to eql application_record_url(@interview.application_record_id) + "\n"
-    expect(@lines['SUMMARY']).to eql @interview.calendar_title + "\n"
-    expect(@lines['UID']).to eql 'INTERVIEW' + @interview.id.to_s + "@UMASS_TRANSIT//JOBAPPS\n"
-    expect(@lines['DTSTAMP']).to eql ((format_date_time DateTime.now, format: :iCal) + "\n")
+    expect(@lines['DTSTART']).to eql (format_date_time @interview.scheduled, format: :iCal)
+    expect(@lines['DESCRIPTION']).to eql application_record_url(@interview.application_record_id)
+    expect(@lines['SUMMARY']).to eql @interview.calendar_title
+    expect(@lines['UID']).to eql 'INTERVIEW' + @interview.id.to_s + "@UMASS_TRANSIT//JOBAPPS"
+    expect(@lines['DTSTAMP']).to eql (format_date_time DateTime.now, format: :iCal)
   end
 end
