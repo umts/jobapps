6d8e7e44438d6d60d473ee703488a4386ab12d9b
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-14 14:31:58 -0500

    Added and completed the position edit spec

---
+++ spec/features/position_edit_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+describe 'editing positions' do
+  let!(:department) { create :department }
+  let!(:base_attributes) do
+    { name: 'A position',
+      department: department,
+      default_interview_location: 'UMTS' }
+  end
+  let!(:position) { create :position, base_attributes }
+  before :each do
+    when_current_user_is :staff, integration: true
+    visit edit_position_path(position)
+  end
+  context 'required fields are filled in' do
+    before :each do
+      within 'form.edit_position' do
+        fill_in 'Name', with: 'The name changed!'
+      end
+    end
+    it 'changes the desired field' do
+      click_on 'Save changes'
+      expect(position.reload.name).to eql 'The name changed!'
+    end
+    it 'redirects to the dashboard' do
+      click_on 'Save changes'
+      expect(page.current_url).to eql staff_dashboard_url
+    end
+    it 'renders a positive flash message' do
+      click_on 'Save changes'
+      expect(page).to have_selector '#message',
+                                    text: 'Position has been updated'
+    end
+  end
+
+  context 'required fields are not filled in' do
+    before :each do
+      within 'form.edit_position' do
+        fill_in 'Name', with: ''
+      end
+    end
+    it 'changes nothing' do
+      click_on 'Save changes'
+      expect(position.reload.name).to eql 'A position'
+    end
+    it 'redirects to the same page' do
+      click_on 'Save changes'
+      expect(page.current_url).to eql edit_position_url(position)
+    end
+    it 'renders a negative flash message' do
+      click_on 'Save changes'
+      expect(page).to have_selector '#errors', text: 'Name can\'t be blank'
+    end
+  end
+end
