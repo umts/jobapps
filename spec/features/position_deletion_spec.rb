0cf80cf4fef3935501a0845200ff643696aca679
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-29 12:23:25 -0500

    moved clicking Delete into a let block

--- spec/features/position_deletion_spec.rb
+++ spec/features/position_deletion_spec.rb
@@ -8,21 +8,22 @@ describe 'deleting a position' do
       default_interview_location: 'UMTS' }
   end
   let!(:position) { create :position, base_attributes }
+  let(:delete) {click_on "Remove #{position.name} (#{position.department.name})"}
   before :each do
     when_current_user_is :staff, integration: true
     visit edit_position_path(position)
   end
   it 'deletes the position in question' do
-    expect { click_on "Remove #{position.name} (#{position.department.name})" }
+    expect { delete }
       .to change { Position.count }.by(-1)
     expect(Position.all).not_to include position
   end
   it 'redirects to the staff dashboard' do
-    click_on "Remove #{position.name} (#{position.department.name})"
+    delete
     expect(page.current_url).to eql staff_dashboard_url
   end
   it 'renders a positive flash message' do
-    click_on "Remove #{position.name} (#{position.department.name})"
+    delete
     expect(page).to have_selector '#message', text: 'Position has been deleted.'
   end
 end
