d8201eb74eaec0c5df9e3e151d6a4dbd9aea148c
Karin Eichelman <keichelman@gmail.com>
2016-03-31 15:18:51 -0400

    a couple todos left but works right now

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,23 +1,21 @@
 class SubscriptionsController < ApplicationController
-  def create
-    @subscription = Subscription.new
 
-  end
   def create
-    binding.pry
     @subscription = Subscription.new subscription_parameters
+    @subscription.save!
     redirect_to :back
   end
 
-    # TODO Make it mail stuff?
-    # TODO add the email value that the user enters to sub.
-    # TODO create new subscription
-    # Subscription.new
-    # user_id = current_user.id - from params?
-    # email = user input
-    # position_id = @position.find params[:id]
-  # redirect to back
+  def destroy
+    params.require :id
+    @subscription = Subscription.find params[:id]
+    @subscription.destroy
+    redirect_to :back
+    #TODO: show flash message (you have been unsubscribed)
+  end
+
   private
+
   def subscription_parameters
     params.require(:subscription).permit :email,
                                          :position_id,
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,10 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :position
+
+  validates :email,
+            :user_id,
+            :position_id, presence: true
+
+  
 end
