0e7d7d7e783c390e197dc2d843ab6bea15d15fe7
Karin Eichelman <keichelman@gmail.com>
2016-03-31 17:14:42 -0400

    Added validation to subscription model
    Subscription cannot have duplicate email addresses for the same
    position.
    that would be silly.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :position
 
+  validates :position_id, uniqueness: { scope: :email }
+
   validates :email,
             :user_id,
             :position_id, presence: true
