3e760d5301b7728cce7811d1e0335ea3b8381fc3
Karin Eichelman <keichelman@gmail.com>
2016-04-22 15:30:14 -0400

    changed dig method to check for nil, changed tests

--- lib/application_configuration.rb
+++ lib/application_configuration.rb
@@ -7,13 +7,10 @@ module ApplicationConfiguration
   # configured.
   def configured_value(config_path, options = {})
     value = CONFIG.dig(*config_path)
-
-    # This is a peculiar situation because value will return {}
-    # on failed queries. However, it is entirely possible that
-    # a query will yield false (as a configured value), so we cannot
-    # use standard .present? or .blank?. We also cannot use .empty?
-    # because TrueClass and FalseClass do not support it
-    if value != {} then value
+    # dig returns nil when key not found
+    # but, it is possible that a query will yeild false
+    # as a configured value, so we cannot use .present?
+    if !value.nil? then value
     elsif options[:default].present? then options[:default]
     else
       raise ArgumentError,
