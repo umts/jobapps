2f4c8c6822ac17230ebcc0d3d60e9cf1f39cca3b
Brian Gregg <bgregg@umass.edu>
2016-05-02 10:52:46 -0400

    migration:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329203210) do
+ActiveRecord::Schema.define(version: 20160224210657) do
 
   create_table "application_drafts", force: :cascade do |t|
     t.integer  "application_template_id", limit: 4
@@ -84,14 +84,6 @@ ActiveRecord::Schema.define(version: 20160329203210) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug",       limit: 255
-  end
-
-  create_table "subscriptions", force: :cascade do |t|
-    t.integer  "user_id",     limit: 4
-    t.integer  "position_id", limit: 4
-    t.string   "email",       limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
