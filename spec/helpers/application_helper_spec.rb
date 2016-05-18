70c14195565f2dd2863f576bf7cd055a948186d1
David Faulkenberry <david.faulkenberry@gmail.com>
2016-05-18 11:14:08 -0400

    Fix application record creation and data retrieval
    Closes #246.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -14,37 +14,28 @@ describe ApplicationHelper do
   end
 
   describe 'parse_application_data' do
-    before :each do
-      @data = {}
-      (0..4).each do |num|
-        %w(prompt response data_type).each do |type|
-          @data["#{type}_#{num}"] = "#{num}-#{type}"
-        end
-        @data[num.to_s] = "#{num}-#{num}"
-      end
-      @result = parse_application_data(@data)
-                .select { |sub| !sub.nil? && sub.all? }
+    let :input do
+      { 'prompt_0'    => 'What is your name?',
+        'response_0'  => 'Luke Starkiller',
+        'data_type_0' => 'text',
+        '316'         => '316-1' }
     end
-
-    it 'does not return nothing when given data' do
-      expect(@result.length).not_to be_zero
+    let(:output) { parse_application_data input }
+    let(:question_data) { output.first }
+    it 'has the same size as the number of questions' do
+      expect(output.length).to be 1
     end
-
-    it 'generates the correct amount of data' do
-      expect(@result.length).to be(5)
-      @result.each do |subarray|
-        expect(subarray.length).to be(4)
-        expect(subarray.count(nil)).to be(0)
-      end
+    it 'puts the prompt in position 0' do
+      expect(question_data[0]).to eql 'What is your name?'
     end
-
-    it 'generates proper data' do
-      @result.each_with_index do |subarray, index|
-        %w(prompt response data_type).each do |type|
-          expect(subarray).to include("#{index}-#{type}")
-        end
-        expect(subarray).to include(index + 1)
-      end
+    it 'puts the response in position 1' do
+      expect(question_data[1]).to eql 'Luke Starkiller'
+    end
+    it 'puts the data type in position 2' do
+      expect(question_data[2]).to eql 'text'
+    end
+    it 'puts the question ID in position 3' do
+      expect(question_data[3]).to be 316
     end
   end
 end
