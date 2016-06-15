1eed93eb7bb67d38fdfea0ae6baf353829777091
Brian Gregg <bgregg@umass.edu>
2016-06-15 14:39:42 -0400

    switched the location of constants, also fixed params a bit

--- app/models/unavailability.rb
+++ app/models/unavailability.rb
@@ -1,5 +1,7 @@
 class Unavailability < ActiveRecord::Base
   belongs_to :application_record
 
+  HOURS = ['7AM','8AM','9AM','10AM', '11AM', '12PM', '1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM']
+  DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 end
