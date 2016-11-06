46d2802c3cd44e23dbc08755e33b03b52296a301
AkivaGreen <green.akiva@outlook.com>
2016-11-05 23:01:13 -0400

    Removing unnecessary migration files after running migration

--- db/migrate/20161103211510_add_notification_to_application_record.rb
+++
@@ -1,5 +0,0 @@
-class AddNotificationToApplicationRecord < ActiveRecord::Migration
-  def change
-    add_column :application_records, :notification, :boolean
-  end
-end
--- db/migrate/20161103211806_remove_notification_from_application_record.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotificationFromApplicationRecord < ActiveRecord::Migration
-  def change
-    remove_column :application_records, :notification, :boolean
-  end
-end
