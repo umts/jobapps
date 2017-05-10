335680033ce79a3885bfa48f36b54b7af8687029
anbranin <keichelman@gmail.com>
2017-05-10 14:10:24 -0400

    4.6 is not a migration version

--- db/migrate/20170308023204_rename_application_record_to_filed_application.rb
+++ db/migrate/20170308023204_rename_application_record_to_filed_application.rb
@@ -1,4 +1,4 @@
-class RenameApplicationRecordToFiledApplication < ActiveRecord::Migration[4.6]
+class RenameApplicationRecordToFiledApplication < ActiveRecord::Migration[4.2]
   def change
     rename_table :application_records, :filed_applications
   end
