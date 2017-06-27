5e280b6c1e3660ca0ed0382d17233b15c65242a7
Matt Moretti <werebus@gmail.com>
2017-06-27 10:58:12 -0400

    "Upgraded" capistrano to 3.8
    Well, actually, it was already updated but without the changes to
    Capfile and config/deploy.rb needed.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :repo_url, 'git@github.com:umts/jobapps.git'
 set :branch, :master
 set :deploy_to, "/srv/#{fetch :application}"
 set :log_level, :info
-set :scm, :git
 set :keep_releases, 5
 
 set :whenever_command, [:sudo, :bundle, :exec, :whenever]
@@ -16,6 +15,4 @@ set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml'
 )
 
-set :linked_dirs, fetch(:linked_dirs, []).push(
-  'log'
-)
+set :linked_dirs, fetch(:linked_dirs, []).push('log')
