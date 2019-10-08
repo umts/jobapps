399ade47f7d10bac42f0610b7792a24181482f0d
Anbranin <keichelman@gmail.com>
2019-10-08 16:18:25 -0400

    change datepicker from coffeescript to javascript and fix options

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  // you must set a dateFormatter for datetimepicker to use or
+  // you'll get an error
+  $.datetimepicker.setDateFormatter('moment');
+
+  $('.datepicker').datetimepicker({
+    timepicker: false,
+    format: 'dddd, MMMM D, YYYY'
+  });
+
+  $('.datetimepicker').datetimepicker({
+    format: 'dddd, MMMM D, YYYY, h:mm a',
+    step: 15
+  });
+});
