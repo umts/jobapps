4921be367712e2f33013e5e0b717d013ec4c622a
Anbranin <keichelman@gmail.com>
2020-02-05 12:54:22 -0500

    don't make changes to these files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
