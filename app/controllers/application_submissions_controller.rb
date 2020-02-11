b7ef903367eb0abe1198fc6136682263cd2e0377
Anbranin <keichelman@gmail.com>
2020-02-11 15:43:29 -0500

    abstract interview params out

--- app/controllers/application_submissions_controller.rb
+++ app/controllers/application_submissions_controller.rb
@@ -56,15 +56,7 @@ class ApplicationSubmissionsController < ApplicationController
 
   def review
     if params[:application_submission][:accepted] == 'true'
-      interview_parameters = params.require(:interview)
-                                   .permit(:location, :scheduled)
-      interview_parameters.require :location
-      interview_parameters.require :scheduled
-      interview_parameters.merge! completed: false,
-                                  hired: false,
-                                  application_submission: @record,
-                                  user: @record.user
-      Interview.create! interview_parameters
+      Interview.create! interview_params
     else @record.deny_with params[:application_submission][:staff_note]
     end
     show_message :application_review,
@@ -147,5 +139,16 @@ class ApplicationSubmissionsController < ApplicationController
       )
     end
     parameters
+  end
+
+  def interview_params
+    interview_parameters = params.require(:interview)
+                                 .permit(:location, :scheduled)
+    interview_parameters.merge(
+      completed: false,
+      hired: false,
+      application_submission: @record,
+      user: @record.user
+    )
   end
 end
