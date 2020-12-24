e5a546e4ff641ecd7a846fac3db7acda4d9c1f45
Matt Moretti <werebus@gmail.com>
2020-12-24 14:34:49 -0500

    Remove autoload paths
    Temporarily require date adn time methods app-wide

--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,11 @@ Bundler.require(*Rails.groups)
 
 module Jobapps
   class Application < Rails::Application
+    require 'date_and_time_methods'
+
     config.load_defaults 5.2
     config.encoding = 'utf-8'
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('vendor/lib')
     config.filter_parameters += [:password, :secret, :spire, :github]
   end
 end
