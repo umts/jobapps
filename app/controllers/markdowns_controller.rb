0efd74d6676c128b7bf40d073ecd3724ac58c01a
Tanvish Jha <tanvishjha@Tanvishs-MacBook-Pro.local>
2021-01-04 20:42:49 -0500

    fixed rubocop error message

--- app/controllers/markdowns_controller.rb
+++ app/controllers/markdowns_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-class MarkdownsController < ApplicationController
-	
-	def explanation; end
-	
-	def edit
-		params.permit :preview_input
-		@preview_input = params[:preview_input]
-  	end
+class MarkdownsController < ApplicationController	
+  def explanation; end
 
+  def edit
+    params.permit :preview_input
+    @preview_input = params[:preview_input]
+  end
+  
 end
