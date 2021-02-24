6ec42d986866dc05ceb9dbea853d3c75c7d905d7
Matt Moretti <werebus@gmail.com>
2021-02-24 14:41:26 -0500

    Ran app update task

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,12 @@ module Jobapps
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.encoding = 'utf-8'
-    config.time_zone = 'Eastern Time (US & Canada)'
-    config.filter_parameters += [:password, :secret, :spire, :github]
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Eastern Time (US & Canada)"
   end
 end
