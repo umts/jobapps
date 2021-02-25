de6104dc7117b4beadb3aeca0a79e6534084613e
Matt Moretti <werebus@gmail.com>
2021-02-25 12:58:20 -0500

    Removed old helper method (and spec)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,32 +3,6 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  describe 'parse_application_data' do
-    let :input do
-      { 'prompt_0' => 'What is your name?',
-        'response_0' => 'Luke Starkiller',
-        'data_type_0' => 'text',
-        '316' => '316-1' }
-    end
-    let(:output) { parse_application_data input }
-    let(:question_data) { output.first }
-    it 'has the same size as the number of questions' do
-      expect(output.length).to be 1
-    end
-    it 'puts the prompt in position 0' do
-      expect(question_data[0]).to eql 'What is your name?'
-    end
-    it 'puts the response in position 1' do
-      expect(question_data[1]).to eql 'Luke Starkiller'
-    end
-    it 'puts the data type in position 2' do
-      expect(question_data[2]).to eql 'text'
-    end
-    it 'puts the question ID in position 3' do
-      expect(question_data[3]).to be 316
-    end
-  end
-
   describe 'parse_unavailability' do
     let :input do
       { 'sunday_7AM' => '0', 'sunday_8AM' => '0', 'tuesday_11AM' => '1',
