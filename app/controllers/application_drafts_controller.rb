7f3c3d3b9f4b7f7dc329ea59633daab14e2202ac
Anbranin <keichelman@gmail.com>
2021-11-26 11:02:22 -0500

    reflect that templates will only have one draft
    when checking in the controller

--- app/controllers/application_drafts_controller.rb
+++ app/controllers/application_drafts_controller.rb
@@ -15,8 +15,7 @@ class ApplicationDraftsController < ApplicationController
 
   def new
     template = ApplicationTemplate.find(params.require :application_template_id)
-    @draft = template.create_draft(@current_user) ||
-             template.draft_belonging_to(@current_user)
+    @draft = template.create_draft(@current_user) || template.draft
     redirect_to edit_draft_path(@draft)
   end
 
