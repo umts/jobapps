d9fc38a78f329aca46fda0912712fea4255e5c2b
Nick Dirschel <dirschn@gmail.com>
2022-11-01 12:42:57 -0400

    use rails credentials

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,3 @@ server 'af-transit-app3.admin.umass.edu',
        ssh_options: { forward_agent: false }
 
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
-set :default_env, { 'SECRET_KEY_BASE' => 'NOT_A_REAL_SECRET_AND_THATS_OK' }
