3359e21757625c1d8a90b69835f7ca654b71557b
Matt Moretti <werebus@gmail.com>
2023-12-27 15:30:46 -0500

    Rubocopped spec/{helpers,lib,support}

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,13 +4,11 @@ require 'rails_helper'
 
 describe ApplicationHelper do
   describe 'render_markdown' do
-    before do
-      @before_markdown_input = '**Bold**'
-      @bold_tag = '<strong>'
-    end
+    let(:input) { '**Bold**' }
+    let(:output) { '<strong>' }
 
     it 'renders markdown for bold text properly' do
-      expect(render_markdown @before_markdown_input).to include @bold_tag
+      expect(render_markdown input).to include(output)
     end
   end
 end
