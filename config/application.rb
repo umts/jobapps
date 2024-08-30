06e8030b78741f665502fd6c7660e81196dbdf44
Matt Moretti <werebus@gmail.com>
2024-08-30 09:21:54 -0400

    Complete 7.0 upgrade (#636)

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,13 @@ Bundler.require(*Rails.groups)
 module Jobapps
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.active_support.cache_format_version = 7.0
-    config.active_support.disable_to_s_conversion = true
     config.time_zone = "Eastern Time (US & Canada)"
   end
 end
