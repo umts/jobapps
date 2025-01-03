e9eb466b44edb19d176ec098e7a60c57f8894273
Matt Moretti <werebus@gmail.com>
2025-01-03 08:28:47 -0500

    Don't use system libraries to build Nokogiri
    Similar to umts/stop-project#368 this doesn't _do_ anything, but if it
    did, it wouldn't work.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
 server 'af-transit-app3.admin.umass.edu',
        roles: %w(app db web),
        ssh_options: { forward_agent: false }
-
-set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
