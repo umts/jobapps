2c0b87ed30b5fb1220fa7700d950c790edbda99d
Matt Moretti <werebus@gmail.com>
2025-09-18 13:48:04 -0400

    Rubocopped interviews controller spec (#839)

--- spec/controllers/interviews_controller_spec.rb
+++ spec/controllers/interviews_controller_spec.rb
@@ -10,63 +10,67 @@ describe InterviewsController do
   ]
 
   describe 'POST #complete' do
-    before do
-      @interview = create(:interview, completed: false)
-    end
+    let(:interview) { create(:interview, completed: false) }
 
     let :submit do
-      post :complete, params: { id: @interview.id }
+      post :complete, params: { id: interview.id }
     end
 
-    context 'staff' do
+    context 'when the current user is staff' do
       before do
         when_current_user_is :staff
       end
 
-      context 'hired button is pressed' do
+      context 'when the "hired" button is pressed' do
         let :submit do
-          post :complete, params: { id: @interview.id, hired: 'anything' }
+          post :complete, params: { id: interview.id, hired: 'anything' }
         end
 
         it 'marks interview as complete' do
           submit
-          expect(@interview.reload).to be_completed
+          expect(interview.reload).to be_completed
         end
 
         it 'markes interview as hired' do
           submit
-          expect(@interview.reload).to be_hired
+          expect(interview.reload).to be_hired
         end
       end
 
-      context 'not hired button is pressed' do
+      context 'when the "hired" button is not pressed' do
         let :submit do
-          post :complete, params: { id: @interview.id, interview_note: 'note' }
+          post :complete, params: { id: interview.id, interview_note: 'note' }
         end
 
         it 'marks the interview as complete' do
           submit
-          expect(@interview.reload).to be_completed
+          expect(interview.reload).to be_completed
         end
 
         it 'marks interview as not hired' do
           submit
-          expect(@interview.reload).not_to be_hired
+          expect(interview.reload).not_to be_hired
         end
 
         it 'adds an interview note to the interview' do
           submit
-          expect(@interview.reload.interview_note).to eql 'note'
+          expect(interview.reload.interview_note).to eql 'note'
         end
       end
 
       context 'with errors' do
-        it 'redirects with errors stored in flash' do
+        before do
           request.env['HTTP_REFERER'] = '/some/path'
-          expect_any_instance_of(Interview)
-            .to receive(:update)
-            .and_return(false)
+          allow(Interview).to receive(:find).and_return(interview)
+          allow(interview).to receive(:update).and_return(false)
+        end
+
+        it 'redirects back' do
           expect(submit).to redirect_to('/some/path')
+        end
+
+        it 'redirects with errors stored in flash' do
+          submit
           # to have_key won't work here,
           # ActionDispatch::Flash::FlashHash doesn't respond to has_key?
           expect(flash.keys).to include 'errors'
@@ -83,32 +87,26 @@ describe InterviewsController do
   end
 
   describe 'POST #reschedule' do
-    before do
-      @interview = create(:interview)
-      @location = 'New location'
-      # beginning_of_day to eliminate failures
-      # based on the seconds not matching up
-      @scheduled = 1.day.since.beginning_of_day
-    end
+    let(:interview) { create(:interview) }
+    let(:location) { 'New location' }
+    let(:scheduled) { 1.day.from_now.change(sec: 0) }
 
     let :submit do
       post :reschedule, params: {
-        id: @interview.id,
-        location: @location,
-        scheduled: @scheduled.strftime('%Y/%m/%d %H:%M')
+        id: interview.id,
+        location: location,
+        scheduled: scheduled.strftime('%Y/%m/%d %H:%M')
       }
     end
 
-    context 'staff' do
+    context 'when the current user is staff' do
       before do
         when_current_user_is :staff
       end
 
       it 'updates interview as specified' do
         submit
-        @interview.reload
-        expect(@interview.location).to eql @location
-        expect(@interview.scheduled).to eql @scheduled
+        expect(interview.reload).to have_attributes(location:, scheduled:)
       end
 
       it 'redirects to staff dashboard' do
@@ -117,12 +115,18 @@ describe InterviewsController do
       end
 
       context 'with errors' do
-        it 'redirects with errors stored in flash' do
+        before do
           request.env['HTTP_REFERER'] = '/some/path'
-          expect_any_instance_of(Interview)
-            .to receive(:update)
-            .and_return(false)
+          allow(Interview).to receive(:find).and_return(interview)
+          allow(interview).to receive(:update).and_return(false)
+        end
+
+        it 'redirects back' do
           expect(submit).to redirect_to('/some/path')
+        end
+
+        it 'redirects with errors stored in flash' do
+          submit
           # to have_key won't work here,
           # ActionDispatch::Flash::FlashHash doesn't respond to has_key?
           expect(flash.keys).to include 'errors'
@@ -132,26 +136,24 @@ describe InterviewsController do
   end
 
   describe 'GET #show' do
-    before do
-      @interview = create(:interview)
-    end
+    let(:interview) { create(:interview) }
 
     let :submit do
-      get :show, params: { id: @interview.id, format: :ics }
+      get :show, params: { id: interview.id, format: :ics }
     end
     let :calendar_fields do
       response.body.lines.to_h { |l| l.strip.split(':', 2) }
     end
 
-    context 'staff' do
+    context 'when the current user is staff' do
       before do
         when_current_user_is :staff
         submit
       end
 
       it 'does not respond to HTML if so requested' do
-        expect { get :show, params: { id: @interview.id, format: :html } }
-          .to raise_error ActionController::UnknownFormat
+        expect { get :show, params: { id: interview.id, format: :html } }
+          .to raise_error(ActionController::UnknownFormat)
       end
 
       it 'renders an ICS file if so requested' do
@@ -159,29 +161,24 @@ describe InterviewsController do
       end
 
       it 'has a UID' do
-        expect(calendar_fields.fetch 'UID')
-          .to eq "INTERVIEW#{@interview.id}@UMASS_TRANSIT//JOBAPPS"
+        expect(calendar_fields.fetch 'UID').to eq("INTERVIEW#{interview.id}@UMASS_TRANSIT//JOBAPPS")
       end
 
       it 'has a start time' do
-        expect(calendar_fields.fetch 'DTSTART')
-          .to eq @interview.scheduled.to_fs :ical
+        expect(calendar_fields.fetch 'DTSTART').to eq(interview.scheduled.to_fs :ical)
       end
 
       it 'has a summary' do
-        expect(calendar_fields.fetch 'SUMMARY')
-          .to eq @interview.calendar_title
+        expect(calendar_fields.fetch 'SUMMARY').to eq(interview.calendar_title)
       end
 
       it 'has a description' do
-        expect(calendar_fields.fetch 'DESCRIPTION').to match(
-          application_submission_path(@interview.application_submission)
-        )
+        expect(calendar_fields.fetch 'DESCRIPTION')
+          .to match(application_submission_path(interview.application_submission))
       end
 
       it 'has a stamp' do
-        expect(calendar_fields.fetch 'DTSTART')
-          .to eq @interview.created_at.to_fs :ical
+        expect(calendar_fields.fetch 'DTSTAMP').to eq(interview.created_at.to_fs :ical)
       end
     end
   end
