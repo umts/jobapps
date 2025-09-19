fc385123ee2468a0a8d62c1d08280e886dd8861e
Matt Moretti <werebus@gmail.com>
2025-09-19 09:15:14 -0400

    Rubocopped ApplicationDraft spec (#843)

--- spec/models/application_draft_spec.rb
+++ spec/models/application_draft_spec.rb
@@ -3,129 +3,108 @@
 require 'rails_helper'
 
 describe ApplicationDraft do
-  describe 'move_question' do
-    before do
-      @draft = create(:application_draft)
-      @question_above = create(:question, application_draft: @draft, number: 1)
-      @question_to_move = create(:question, application_draft: @draft, number: 2)
-      @question_below = create(:question, application_draft: @draft, number: 3)
-      @number = @question_to_move.number
+  let(:draft) { create(:application_draft) }
+
+  describe '#move_question' do
+    subject :call do
+      draft.move_question question_to_move.number, direction
     end
 
-    let :call do
-      @draft.move_question @number, @direction
-    end
+    let!(:question_above) { create(:question, application_draft: draft, number: 1) }
+    let(:question_to_move) { create(:question, application_draft: draft, number: 2) }
+    let!(:question_below) { create(:question, application_draft: draft, number: 3) }
 
-    context 'direction is up' do
-      before do
-        @direction = :up
-      end
+    context 'when moving a question up' do
+      let(:direction) { :up }
 
       it 'moves the question up by 1' do
-        expect { call }
-          .to change { @question_to_move.reload.number }
-          .by(-1)
+        expect { call }.to change { question_to_move.reload.number }.by(-1)
       end
 
       it 'moves the question above down by 1' do
-        expect { call }
-          .to change { @question_above.reload.number }
-          .by 1
+        expect { call }.to change { question_above.reload.number }.by(1)
       end
 
       it 'does not change the question below' do
-        expect { call }
-          .not_to change { @question_below.reload.number }
+        expect { call }.not_to change { question_below.reload.number }
       end
     end
 
-    context 'direction is down' do
-      before do
-        @direction = :down
-      end
+    context 'when moving a question down' do
+      let(:direction) { :down }
 
       it 'moves the question in question down by 1' do
-        expect { call }
-          .to change { @question_to_move.reload.number }
-          .by 1
+        expect { call }.to change { question_to_move.reload.number }.by(1)
       end
 
       it 'moves the question below up by 1' do
-        expect { call }
-          .to change { @question_below.reload.number }
-          .by(-1)
+        expect { call }.to change { question_below.reload.number }.by(-1)
       end
 
       it 'does not change the question above' do
-        expect { call }
-          .not_to change { @question_above.reload.number }
+        expect { call }.not_to change { question_above.reload.number }
       end
     end
   end
 
-  describe 'new_question' do
+  describe '#new_question' do
+    subject(:call) { draft.new_question }
+
     before do
-      @draft = create(:application_draft)
-      # Create three existing questions - the number of the new one should be 4
-      create(:question, application_draft: @draft, number: 1)
-      create(:question, application_draft: @draft, number: 2)
-      create(:question, application_draft: @draft, number: 3)
+      create_list(:question, 3, application_draft: draft) do |question, i|
+        question.update!(number: i + 1)
+      end
     end
 
-    let :call do
-      @draft.new_question
+    it { is_expected.to be_new_record }
+
+    it 'initializes a new question for the draft' do
+      expect(call.application_draft).to eq(draft)
     end
 
-    it 'initializes a new question for the draft with the correct number' do
-      expect(call.application_draft).to eql @draft
-      expect(call.number).to be 4
-      expect(call).to be_new_record
+    it 'initializes a new question with the correct number' do
+      expect(call.number).to be(4)
     end
   end
 
-  describe 'remove_question' do
-    before do
-      @draft = create(:application_draft)
-      @first_question  = create(:question, application_draft: @draft)
-      @second_question = create(:question, application_draft: @draft)
-      @third_question  = create(:question, application_draft: @draft)
+  describe '#remove_question' do
+    subject :call do
+      draft.remove_question question_to_remove.number
     end
 
-    let :call do
-      @draft.remove_question @second_question.number
-    end
+    let!(:question_above) { create(:question, application_draft: draft, number: 1) }
+    let(:question_to_remove) { create(:question, application_draft: draft, number: 2) }
+    let!(:question_below) { create(:question, application_draft: draft, number: 3) }
 
     it 'removes the specified question' do
       call
-      expect(@draft.reload.questions).not_to include @second_question
+      expect(draft.reload.questions).not_to include(question_to_remove)
     end
 
     it 'does not change the number of any questions above the one specified' do
-      expect { call }
-        .not_to change { @first_question.reload.number }
+      expect { call }.not_to change { question_above.reload.number }
     end
 
     it 'moves questions below the specified question up by one' do
-      expect { call }
-        .to change { @third_question.reload.number }
-        .by(-1)
+      expect { call }.to change { question_below.reload.number }.by(-1)
     end
   end
 
-  describe 'update_questions' do
-    before do
-      @draft = create(:application_draft)
-      @question = create(:question, application_draft: @draft)
-      @new_prompt = 'A new prompt'
-      @question_data = [
+  describe '#update_questions' do
+    subject(:call) { draft.update_questions question_data }
+
+    let(:question) { create(:question, application_draft: draft) }
+    let(:new_prompt) { 'A new prompt' }
+    let(:question_data) do
+      [
         {
-          number: @question.number,
-          prompt: 'A new prompt',
-          data_type: @question.data_type,
-          required: @question.required
+          number: question.number,
+          prompt: new_prompt,
+          data_type: question.data_type,
+          required: question.required
         },
         {
-          number: @question.number + 1,
+          number: question.number + 1,
           prompt: 'A prompt',
           data_type: 'text',
           required: true
@@ -133,42 +112,38 @@ describe ApplicationDraft do
       ]
     end
 
-    let :call do
-      @draft.update_questions @question_data
+    it 'erases old questions' do
+      call
+      expect(draft.questions).not_to include(question)
     end
 
-    it 'erases old questions and creates new ones with the given data' do
+    it 'creates new ones with the given data' do
       call
-      expect(@draft.questions.where(prompt: 'A new prompt')).to exist
-      expect(@draft.questions.where(prompt: 'A prompt')).to exist
+      expect(draft.questions.map(&:prompt)).to contain_exactly('A new prompt', 'A prompt')
     end
   end
 
   describe 'update_application_template!' do
-    before do
-      @template = create(:application_template)
-      @draft = create(:application_draft, application_template: @template)
-      @template_question = create(:question, application_template: @template)
-      @draft_question = create(:question, application_draft: @draft)
-    end
+    subject(:call) { draft.update_application_template! }
 
-    let :call do
-      @draft.update_application_template!
-    end
+    let(:template) { create(:application_template) }
+    let(:draft) { create(:application_draft, application_template: template) }
 
     it 'deletes all the questions in the template' do
+      template_question = create(:question, application_template: template)
       call
-      expect(@template.reload.questions).not_to include @template_question
+      expect(template.reload.questions).not_to include(template_question)
     end
 
     it 'moves the draft questions over to the template' do
+      draft_question = create(:question, application_draft: draft)
       call
-      expect(@template.reload.questions).to include @draft_question
+      expect(template.reload.questions).to include(draft_question)
     end
 
     it 'destroys itself' do
       call
-      expect(described_class.where id: @draft.id).to be_empty
+      expect(described_class.where id: draft.id).to be_empty
     end
   end
 end
