01a252ef69f194e69b55b8b777f8f5a7d9258b2b
Matt Moretti <werebus@gmail.com>
2025-09-19 12:29:22 -0400

    Rubocop remaining model specs (#847)
    * Rubocop Position spec
    
    * Rubocop Question spec
    
    * Rubocop User spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,105 +4,78 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe Question do
-  context 'validations' do
-    # Yes that's right, ladies and gentlemen,
-    # we are testing an XOR gate.
-    before do
-      @draft = create(:application_draft)
-      @template = create(:application_template)
+  describe 'validations' do
+    let(:draft) { create(:application_draft) }
+    let(:template) { create(:application_template) }
+
+    it 'does not permit a question to belong to neither a draft or a template' do
+      question = build(:question, application_draft: nil, application_template: nil)
+      expect(question).not_to be_valid
     end
 
-    context 'question belonging to neither draft nor template' do
-      it 'fails' do
-        question = build(:question,
-                         application_draft: nil,
-                         application_template: nil)
-        expect { question.save! }.to raise_error ActiveRecord::RecordInvalid
-      end
+    it 'allows a question belonging only to a draft' do
+      question = build(:question, application_draft: draft, application_template: nil)
+      expect(question).to be_valid
     end
 
-    context 'question belonging to draft but not template' do
-      it 'passes' do
-        question = build(:question,
-                         application_draft: @draft,
-                         application_template: nil)
-        expect { question.save! }.not_to raise_error
-      end
+    it 'allows a question belonging only to a template' do
+      question = build(:question, application_draft: nil, application_template: template)
+      expect(question).to be_valid
     end
 
-    context 'questions belonging to template but not draft' do
-      it 'passes' do
-        question = build(:question,
-                         application_draft: nil,
-                         application_template: @template)
-        expect { question.save! }.not_to raise_error
-      end
-    end
-
-    context 'question belonging to both draft and template' do
-      it 'fails' do
-        question = build(:question,
-                         application_draft: @draft,
-                         application_template: @template)
-        expect { question.save! }.to raise_error ActiveRecord::RecordInvalid
-      end
+    it 'does not permit a question beloning to both a draft and a template' do
+      question = build(:question, application_draft: draft, application_template: template)
+      expect(question).not_to be_valid
     end
   end
 
-  context 'data_type identification methods' do
-    before do
-      @template = create(:application_template)
-      @date = create(:question, data_type: 'date', application_template: @template)
-      @explanation = create(:question, data_type: 'explanation', application_template: @template)
-      @heading = create(:question, data_type: 'heading', application_template: @template)
+  describe '#date?' do
+    it 'returns true if data type is date' do
+      question = build(:question, data_type: 'date')
+      expect(question).to be_date
     end
 
-    describe 'date?' do
-      it 'returns true if data type is date' do
-        expect(@date).to be_date
-      end
-
-      it 'returns false if data type is not date' do
-        expect(@explanation).not_to be_date
-      end
-    end
-
-    describe 'explanation?' do
-      it 'returns true if data type is explanation' do
-        expect(@explanation).to be_explanation
-      end
-
-      it 'returns false if data type is not explanation' do
-        expect(@heading).not_to be_explanation
-      end
-    end
-
-    describe 'heading?' do
-      it 'returns true if data type is heading' do
-        expect(@heading).to be_heading
-      end
-
-      it 'returns false if data type is not heading' do
-        expect(@date).not_to be_heading
-      end
+    it 'returns false if data type is not date' do
+      question = build(:question, data_type: 'explanation')
+      expect(question).not_to be_date
     end
   end
 
-  describe 'takes_placeholder?' do
-    before do
-      @draft = create(:application_draft)
+  describe '#explanation?' do
+    it 'returns true if data type is explanation' do
+      question = build(:question, data_type: 'explanation')
+      expect(question).to be_explanation
     end
 
-    it 'returns true for date, or text' do
-      %w[date text].each do |data_type|
-        question = create(:question, application_draft: @draft, data_type:)
-        expect(question.takes_placeholder?).to be true
+    it 'returns false if data type is not explanation' do
+      question = build(:question, data_type: 'heading')
+      expect(question).not_to be_explanation
+    end
+  end
+
+  describe '#heading?' do
+    it 'returns true if data type is heading' do
+      question = build(:question, data_type: 'heading')
+      expect(question).to be_heading
+    end
+
+    it 'returns false if data type is not heading' do
+      question = build(:question, data_type: 'date')
+      expect(question).not_to be_heading
+    end
+  end
+
+  describe '#takes_placeholder?' do
+    %w[date text].each do |data_type|
+      it "returns true for #{data_type}" do
+        question = build(:question, data_type:)
+        expect(question.takes_placeholder?).to be(true)
       end
     end
 
-    it 'returns false for explanation, heading, number, or yes/no' do
-      %w[explanation heading number yes/no].each do |data_type|
-        question = create(:question, application_draft: @draft, data_type:)
+    %w[explanation heading number yes/no].each do |data_type|
+      it "returns false for #{data_type}" do
+        question = build(:question, data_type:)
         expect(question.takes_placeholder?).to be false
       end
     end
