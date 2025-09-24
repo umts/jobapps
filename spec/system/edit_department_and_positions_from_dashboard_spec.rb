57ad843e1390ae49540cd29ee3fd765c199e7023
Matt Moretti <werebus@gmail.com>
2025-09-24 09:13:48 -0400

    Rubocopped department and position system specs (#865)

--- spec/system/edit_department_and_positions_from_dashboard_spec.rb
+++ spec/system/edit_department_and_positions_from_dashboard_spec.rb
@@ -12,24 +12,22 @@ describe 'adding or editing departments and positions from dashboard' do
   end
 
   it 'has a link to edit the department name' do
-    click_link 'Change department name',
-               href: edit_department_path(department)
-    expect(page.current_path).to eql edit_department_path(department)
+    click_link 'Change department name', href: edit_department_path(department)
+    expect(page).to have_current_path(edit_department_path(department))
   end
 
   it 'has a link to edit the positions' do
-    click_link "Edit #{position.name} position",
-               href: edit_position_path(position)
-    expect(page.current_path).to eql edit_position_path(position)
+    click_link "Edit #{position.name} position", href: edit_position_path(position)
+    expect(page).to have_current_path(edit_position_path(position))
   end
 
   it 'has a link to add a new position' do
     click_link 'Add new position', href: new_position_path
-    expect(page.current_path).to eql new_position_path
+    expect(page).to have_current_path(new_position_path)
   end
 
   it 'has a link to add a new department' do
     click_link 'Add new department', href: new_department_path
-    expect(page.current_path).to eql new_department_path
+    expect(page).to have_current_path(new_department_path)
   end
 end
