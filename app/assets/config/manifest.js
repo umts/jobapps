f04e4d939a2463792f381a8af9c92fcbca0f0452
benmelz <ben@melz.me>
2025-09-24 09:32:16 -0400

    adjust bundle

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
-//= link_tree ../images
-//= link application.css
-//= link application.js
+//= link_tree ../../../node_modules/jquery-ui-dist/images
 //= link_tree ../builds
+//= link_tree ../images
+//= link application.js
