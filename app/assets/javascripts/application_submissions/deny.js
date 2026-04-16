e78eb401607ef1da92b3ff2e3991c8daefccfaf8
Matt Moretti <werebus@gmail.com>
2026-04-16 13:07:08 -0400

    Use jQuery's checked property
    Co-authored-by: benmelz <ben@melz.me>

--- app/assets/javascripts/application_submissions/deny.js
+++ app/assets/javascripts/application_submissions/deny.js
@@ -1,6 +1,6 @@
 $(function() {
   $('form').on('change', '#application_submission_notify_of_denial', function(event) {
-    const inform = $(event.target).checked;
+    const inform = $(event.target).prop('checked');
     $('.rejection-message').toggle(inform);
   });
 });
