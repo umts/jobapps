9ef76782aa74da23f15ea78d90516fc079122c24
benmelz <ben@melz.me>
2026-08-26 13:01:47 -0400

    append tenant id

--- app/tasks/maintenance/import_entra_uids_task.rb
+++ app/tasks/maintenance/import_entra_uids_task.rb
@@ -6,8 +6,12 @@ module Maintenance
     csv_collection
 
     def process(row)
-      User.find_by!(spire: "#{row['spire_id']}@umass.edu").update!(entra_uid: row['entra_uid'])
+      User.find_by!(spire: "#{row['spire_id']}@umass.edu").update!(entra_uid: "#{row['entra_uid']}#{tenant_id}")
     end
+
+    private
+
+    def tenant_id = Rails.application.credentials.entra_id.tenant_id
   end
 end
 # simplecov:enable
