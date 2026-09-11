47fbf07f555bd52525f295b19698073d204d25ef
benmelz <ben@melz.me>
2026-09-11 12:01:49 -0400

    Set up jsbundling (#1230)

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,27 @@
-import js from '@eslint/js';
-import {defineConfig, globalIgnores} from 'eslint/config';
-import googleConfig from 'eslint-config-google';
-import globals from 'globals';
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import googleConfig from "eslint-config-google";
+import globals from "globals";
 
-delete googleConfig.rules['valid-jsdoc'];
-delete googleConfig.rules['require-jsdoc'];
+delete googleConfig.rules["valid-jsdoc"];
+delete googleConfig.rules["require-jsdoc"];
 
 export default defineConfig([
-  globalIgnores([
-    'app/assets/builds/*',
-    'coverage/*',
-    'node_modules/*',
-    'public/assets/*',
-  ]),
+  globalIgnores(["app/assets/builds/*", "coverage/*", "node_modules/*", "public/assets/*"]),
   {
-    files: ['**/*.js'],
+    files: ["app/assets/javascripts/**/*.js"],
     ...js.configs.recommended,
     ...googleConfig,
     rules: {
       ...js.configs.recommended.rules,
       ...googleConfig.rules,
-      'max-len': ['error', {code: 120}],
+      "max-len": ["error", { code: 120 }],
     },
-  },
-  {
-    files: ['app/assets/javascripts/**/*.js'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.jquery,
       },
-    },
-  },
-  {
-    files: ['*.config.js'],
-    languageOptions: {
-      globals: {...globals.node},
     },
   },
 ]);
