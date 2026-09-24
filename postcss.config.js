27d7e6db321b396f82e9fea5a00debda12eedadb
benmelz <ben@melz.me>
2026-09-24 11:13:30 -0400

    Configure bootstrap, umass branding and dev login (#1245)
    ---------
    
    Co-authored-by: KernSharma <kern.sharma@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 export default {
   parser: "postcss-scss",
   plugins: {
-    "@csstools/postcss-sass": { includePaths: ["node_modules"], quietDeps: true },
+    "@csstools/postcss-sass": {
+      includePaths: ["node_modules"],
+      quietDeps: true,
+      silenceDeprecations: ["import", "legacy-js-api"],
+    },
     autoprefixer: {},
   },
 };
